b7d333ad5e344e0b1389a4997e5cbef6fcfaeb2c
Dang Ha Minh Tuan <tuandangha2213764@gmail.com>
2025-11-25 00:29:29 +0700

    Update FW ver 2

---
+++ FW_ver2/MDK-ARM/New_control/motor_control.d
@@ -0,0 +1,4 @@
+new_control\motor_control.o: Motor_control_ert_rtw\Motor_control.c
+new_control\motor_control.o: Motor_control_ert_rtw\Motor_control.h
+new_control\motor_control.o: C:\Users\Admin\AppData\Local\Keil_v5\ARM\ARM_Compiler_5.06u7\Bin\..\include\math.h
+new_control\motor_control.o: Motor_control_ert_rtw\rtwtypes.h
